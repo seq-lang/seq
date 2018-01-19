e6f90d47b8950266a37541042cfc900057c93efe
A. R. Shajii <ars@ars.me>
2018-01-18 19:42:12 -0500

    Formatting

--- src/util.h
+++ src/util.h
@@ -4,17 +4,15 @@
 #include <cstdint>
 
 namespace seq {
-namespace util {
-
-	extern "C" bool seq_eq(char *seq1,
-	                       uint32_t len1,
-	                       char *seq2,
-	                       uint32_t len2);
-	extern "C" char revcomp_base(char base);
-	extern "C" void revcomp(char *seq, uint32_t len);
-	extern "C" void dump(char *seq, uint32_t len);
-
-}
+	namespace util {
+		extern "C" bool seq_eq(char *seq1,
+		                       uint32_t len1,
+		                       char *seq2,
+		                       uint32_t len2);
+		extern "C" char revcomp_base(char base);
+		extern "C" void revcomp(char *seq, uint32_t len);
+		extern "C" void dump(char *seq, uint32_t len);
+	}
 }
 
 #endif /* SEQ_UTIL_H */
