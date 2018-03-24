700af264218edcfada8aaebc848ecf9dc7ee3dd4
A. R. Shajii <ars@ars.me>
2018-03-24 18:20:28 -0400

    Add seq_t and arr_t structs

--- include/common.h
+++ include/common.h
@@ -10,6 +10,16 @@ namespace seq {
 
 	typedef int64_t seq_int_t;
 
+	typedef struct {
+		seq_int_t len;
+		char *seq;
+	} seq_t;
+
+	typedef struct {
+		seq_int_t len;
+		void *arr;
+	} arr_t;
+
 	inline llvm::Type *seqIntLLVM(llvm::LLVMContext& context)
 	{
 		return llvm::IntegerType::getIntNTy(context, 8*sizeof(seq_int_t));
