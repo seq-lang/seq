b4228248fa65370e231125b473f3659f5df84a55
A. R. Shajii <ars@ars.me>
2019-10-24 00:58:01 -0400

    Bump patch version

--- docs/sphinx/conf.py
+++ docs/sphinx/conf.py
@@ -30,7 +30,7 @@ author = u'seq-lang'
 # The short X.Y version
 version = u'0.8'
 # The full version, including alpha/beta/rc tags
-release = u'0.8.4'
+release = u'0.8.5'
 
 # Logo path
 html_logo = '../images/logo.png'
