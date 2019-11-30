ecdfaf7e7cbb7225effc0724bf6ceb498dbadabe
A. R. Shajii <ars@ars.me>
2019-11-30 16:50:23 -0500

    Refactor pipelines

--- compiler/include/seq/expr.h
+++ compiler/include/seq/expr.h
@@ -593,22 +593,6 @@ public:
   TypeOfExpr *clone(Generic *ref) override;
 };
 
-class PipeExpr : public Expr {
-private:
-  std::vector<Expr *> stages;
-  std::vector<bool> parallel;
-
-public:
-  static const unsigned SCHED_WIDTH = 16;
-  explicit PipeExpr(std::vector<Expr *> stages,
-                    std::vector<bool> parallel = {});
-  void setParallel(unsigned which);
-  void resolveTypes() override;
-  llvm::Value *codegen0(BaseFunc *base, llvm::BasicBlock *&block) override;
-  types::Type *getType0() const override;
-  PipeExpr *clone(Generic *ref) override;
-};
-
 } // namespace seq
 
 #endif /* SEQ_EXPR_H */
