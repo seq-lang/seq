8e5c475fda597b065b3af1ba042e2281f2086649
jodiew <jodiew@uvic.ca>
2019-12-03 12:18:02 -0800

    Switch to setuptools

---
+++ jupyter/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+from seq_kernel.kernel import __version__
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='seq_kernel',
+    version=__version__,
+    description='Seq Kernel',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/seq-lang/seq',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Framework :: Jupyter',
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Seq',
+    ]
+)
