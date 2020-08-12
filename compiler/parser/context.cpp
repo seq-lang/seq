a2868b6ff5b2ff7f0bf423f8ad78b3e25ad201a4
Ibrahim Numanagić <inumanag@mit.edu>
2020-08-12 09:56:13 -0700

    Fix ORIGIN

--- compiler/parser/context.cpp
+++ compiler/parser/context.cpp
@@ -198,7 +198,7 @@ string ImportCache::getImportFile(const string &what, const string &relativeTo,
     paths.push_back(format("{}/{}/__init__.seq", abs, what));
   }
   if (argv0 != "") {
-    for (auto loci: {"../lib/stdlib", "../stdlib", "stdlib"}) {
+    for (auto loci: {"../lib/seq/stdlib", "../stdlib", "stdlib"}) {
       strncpy(abs, executable_path(argv0.c_str()).c_str(), PATH_MAX);
       auto parent = format("{}/{}", dirname(abs), loci);
       realpath(parent.c_str(), abs);
