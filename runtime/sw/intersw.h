5b96bfb0745178753db9a09565e6bb552dbf4ea5
A. R. Shajii <ars@ars.me>
2020-10-26 19:46:45 -0400

    Dispatch SIMD functions manually to support Clang <7

--- runtime/sw/intersw.h
+++ runtime/sw/intersw.h
@@ -57,8 +57,7 @@ struct SeqPair {
 #define DUMMY2 100
 
 #ifndef __SSE4_1__
-static ALWAYS_INLINE __m128i _mm_blendv_epi8(__m128i x, __m128i y,
-                                             __m128i mask) {
+static inline __m128i _mm_blendv_epi8(__m128i x, __m128i y, __m128i mask) {
   // Replace bit in x with bit in y when matching bit in mask is set:
   return _mm_or_si128(_mm_andnot_si128(mask, x), _mm_and_si128(mask, y));
 }
