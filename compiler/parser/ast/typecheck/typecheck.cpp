02e65d6ea3bffa773dd3a4de95c99b0b4e2d17c7
Ibrahim Numanagić <inumanag@uvic.ca>
2020-12-11 10:18:08 -0800

    Refactor standard library; Add generic LLVM code identifiers

--- compiler/parser/ast/typecheck/typecheck.cpp
+++ compiler/parser/ast/typecheck/typecheck.cpp
@@ -1640,6 +1640,7 @@ vector<types::Generic> TypecheckVisitor::parseGenerics(const vector<Param> &gene
 
 void TypecheckVisitor::addFunctionGenerics(FuncTypePtr t) {
   int pi = 0;
+  LOG("{}", t->realizeString());
   for (auto p = t->parent; p; pi++) {
     if (auto y = p->getFunc()) {
       for (auto &g : y->explicits)
@@ -1652,9 +1653,11 @@ void TypecheckVisitor::addFunctionGenerics(FuncTypePtr t) {
       auto c = p->getClass();
       assert(c);
       for (auto &g : c->explicits)
-        if (auto s = g.type->getStatic())
+        if (auto s = g.type->getStatic()) {
+          LOG("--{}", g.name);
+
           ctx->add(TypecheckItem::Type, g.name, s, false, false, true);
-        else if (!g.name.empty())
+        } else if (!g.name.empty())
           ctx->add(TypecheckItem::Type, g.name, g.type, true);
       p = c->parent;
     }
