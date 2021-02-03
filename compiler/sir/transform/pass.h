7333e0cd02377d0588b256844e70f50e40d8ea66
Gabriel L Ramirez <gabe.luis.ramirez@gmail.com>
2021-02-03 16:19:14 -0700

    refactored

--- compiler/sir/transform/pass.h
+++ compiler/sir/transform/pass.h
@@ -1,15 +1,19 @@
 #pragma once
 
-#include "sir/module.h"
+#include "manager.h"
 
+#include "sir/module.h"
 #include "sir/util/lambda_visitor.h"
 
 namespace seq {
 namespace ir {
-namespace passes {
+namespace transform {
 
 /// General pass base class.
 class Pass {
+private:
+  PassManager *manager;
+
 public:
   virtual ~Pass() = default;
 
@@ -17,8 +21,13 @@ public:
   /// @param module the module
   virtual void run(IRModule *module) = 0;
 
-  /// @return true if this is an analysis pass
-  virtual bool isAnalysis() const { return false; }
+  /// Sets the manager.
+  /// @param mng the new manager
+  void setManager(PassManager *mng) { manager = mng; }
+  template <typename AnalysisType>
+  const AnalysisType *getAnalysisResult(const std::string &key) {
+    return static_cast<const AnalysisType *>(manager->getAnalysisResult(key));
+  }
 };
 
 /// Pass that runs a single LambdaValueVisitor.
@@ -27,18 +36,6 @@ public:
   void run(IRModule *module) override { process(module); }
 };
 
-/// Analysis pass base class.
-class Analysis : public Pass {
-public:
-  void run(IRModule *module) override { runAnalysis(module); }
-
-  bool isAnalysis() const override { return true; }
-
-  /// Execute the analysis.
-  /// @param module the module
-  virtual void runAnalysis(const IRModule *module) = 0;
-};
-
-} // namespace passes
+} // namespace transform
 } // namespace ir
 } // namespace seq
