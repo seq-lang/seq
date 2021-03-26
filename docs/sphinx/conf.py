ba6e8c7efdf214c27d6bdfbe6ca3e47d39e3431d
A. R. Shajii <ars@ars.me>
2021-03-26 12:51:19 -0400

    Bump patch version

--- docs/sphinx/conf.py
+++ docs/sphinx/conf.py
@@ -31,7 +31,7 @@ author = u'seq-lang'
 # The short X.Y version
 version = u'0.10'
 # The full version, including alpha/beta/rc tags
-release = u'0.10.0'
+release = u'0.10.1'
 
 # Logo path
 html_logo = '../images/logo.png'
