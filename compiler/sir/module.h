5617046747bb9c4de377f9671c74d40c6258decf
Gabriel L Ramirez <gabe.luis.ramirez@gmail.com>
2021-04-25 23:23:46 -0600

    undone changes

--- compiler/sir/module.h
+++ compiler/sir/module.h
@@ -203,7 +203,7 @@ public:
   /// @param args the arguments
   /// @return the new node
   template <typename DesiredType, typename... Args>
-  DesiredType *N(seq::SrcInfo s, Args &&... args) {
+  DesiredType *N(seq::SrcInfo s, Args &&...args) {
     auto *ret = new DesiredType(std::forward<Args>(args)...);
     ret->setModule(this);
     ret->setSrcInfo(s);
@@ -216,7 +216,7 @@ public:
   /// @param args the arguments
   /// @return the new node
   template <typename DesiredType, typename... Args>
-  DesiredType *N(const seq::SrcObject *s, Args &&... args) {
+  DesiredType *N(const seq::SrcObject *s, Args &&...args) {
     return N<DesiredType>(s->getSrcInfo(), std::forward<Args>(args)...);
   }
   /// Constructs and registers an IR node with provided source node.
@@ -224,13 +224,13 @@ public:
   /// @param args the arguments
   /// @return the new node
   template <typename DesiredType, typename... Args>
-  DesiredType *N(const Node *s, Args &&... args) {
+  DesiredType *N(const Node *s, Args &&...args) {
     return N<DesiredType>(s->getSrcInfo(), std::forward<Args>(args)...);
   }
   /// Constructs and registers an IR node with no source information.
   /// @param args the arguments
   /// @return the new node
-  template <typename DesiredType, typename... Args> DesiredType *Nr(Args &&... args) {
+  template <typename DesiredType, typename... Args> DesiredType *Nr(Args &&...args) {
     return N<DesiredType>(seq::SrcInfo(), std::forward<Args>(args)...);
   }
 
