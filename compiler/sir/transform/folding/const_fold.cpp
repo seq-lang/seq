c556f8933b195abf0f7119812db2d3070cb85fa4
Gabriel Ramirez <gabe.luis.ramirez@gmail.com>
2021-05-07 01:23:16 -0500

    lots of integer rules

--- compiler/sir/transform/folding/const_fold.cpp
+++ compiler/sir/transform/folding/const_fold.cpp
@@ -2,12 +2,15 @@
 
 #include "sir/util/cloning.h"
 
+#define BINOP(o)                                                                       \
+  [](auto x, auto y) -> auto { return x o y; }
+#define UNOP(o)                                                                        \
+  [](auto x) -> auto { return o x; }
+
 namespace {
 using namespace seq::ir;
 
-template <typename... Args>
-std::unique_ptr<transform::folding::SingleConstantCommutativeRule<int64_t>>
-intSingleRule(Module *m, Args &&...args) {
+template <typename... Args> auto intSingleRule(Module *m, Args &&...args) {
   return std::make_unique<transform::folding::SingleConstantCommutativeRule<int64_t>>(
       std::forward<Args>(args)..., m->getIntType());
 }
@@ -18,6 +21,36 @@ auto identity(Module *m) {
     return cv.clone(v);
   };
 }
+
+template <typename T, typename Func>
+auto doubleConstantBinaryRule(Func f, std::string magic, types::Type *input,
+                              types::Type *output) {
+  return std::make_unique<transform::folding::DoubleConstantBinaryRule<T, Func>>(
+      std::move(f), std::move(magic), input, output);
+}
+
+template <typename Func> auto intToIntBinary(Module *m, Func f, std::string magic) {
+  return doubleConstantBinaryRule<int64_t>(f, std::move(magic), m->getIntType(),
+                                           m->getIntType());
+}
+
+template <typename Func> auto intToBoolBinary(Module *m, Func f, std::string magic) {
+  return doubleConstantBinaryRule<int64_t>(f, std::move(magic), m->getIntType(),
+                                           m->getBoolType());
+}
+
+template <typename T, typename Func>
+auto singleConstantUnaryRule(Func f, std::string magic, types::Type *input,
+                             types::Type *output) {
+  return std::make_unique<transform::folding::SingleConstantUnaryRule<T, Func>>(
+      std::move(f), std::move(magic), input, output);
+}
+
+template <typename Func> auto intToIntUnary(Module *m, Func f, std::string magic) {
+  return singleConstantUnaryRule<int64_t>(f, std::move(magic), m->getIntType(),
+                                          m->getIntType());
+}
+
 } // namespace
 
 namespace seq {
@@ -39,21 +72,56 @@ void FoldingPass::handle(CallInstr *v) {
 }
 
 void FoldingPass::registerStandardRules(Module *m) {
+  // binary, single constant, int->int
   registerRule("int-multiply-by-zero",
                intSingleRule(m, 0, 0, Module::MUL_MAGIC_NAME, -1));
   registerRule("int-multiply-by-one",
-               intSingleRule(m, 1, Module::MUL_MAGIC_NAME, identity(m), -1));
+               intSingleRule(m, 1, identity(m), Module::MUL_MAGIC_NAME, -1));
   registerRule("int-subtract-zero",
-               intSingleRule(m, 0, Module::SUB_MAGIC_NAME, identity(m), 1));
+               intSingleRule(m, 0, identity(m), Module::SUB_MAGIC_NAME, 1));
   registerRule("int-add-zero",
-               intSingleRule(m, 0, Module::ADD_MAGIC_NAME, identity(m), -1));
+               intSingleRule(m, 0, identity(m), Module::ADD_MAGIC_NAME, -1));
   registerRule("int-floor-div-by-one",
-               intSingleRule(m, 1, Module::FLOOR_DIV_MAGIC_NAME, identity(m), 1));
+               intSingleRule(m, 1, identity(m), Module::FLOOR_DIV_MAGIC_NAME, 1));
   registerRule("int-zero-floor-div",
                intSingleRule(m, 0, 0, Module::FLOOR_DIV_MAGIC_NAME, 0));
+
+  // binary, double constant, int->int
+  registerRule("int-constant-addition",
+               intToIntBinary(m, BINOP(+), Module::ADD_MAGIC_NAME));
+  registerRule("int-constant-subtraction",
+               intToIntBinary(m, BINOP(-), Module::SUB_MAGIC_NAME));
+  registerRule("int-constant-floor-div",
+               intToIntBinary(m, BINOP(/), Module::FLOOR_DIV_MAGIC_NAME));
+  registerRule("int-constant-mul", intToIntBinary(m, BINOP(*), Module::MUL_MAGIC_NAME));
+  registerRule("int-constant-lshift",
+               intToIntBinary(m, BINOP(<<), Module::LSHIFT_MAGIC_NAME));
+  registerRule("int-constant-rshift",
+               intToIntBinary(m, BINOP(>>), Module::RSHIFT_MAGIC_NAME));
+  registerRule("int-constant-xor", intToIntBinary(m, BINOP(^), Module::XOR_MAGIC_NAME));
+  registerRule("int-constant-or", intToIntBinary(m, BINOP(|), Module::OR_MAGIC_NAME));
+  registerRule("int-constant-and", intToIntBinary(m, BINOP(&), Module::AND_MAGIC_NAME));
+  registerRule("int-constant-mod", intToIntBinary(m, BINOP(%), Module::MOD_MAGIC_NAME));
+
+  // binary, double constant, int->bool
+  registerRule("int-constant-eq", intToBoolBinary(m, BINOP(==), Module::EQ_MAGIC_NAME));
+  registerRule("int-constant-ne", intToBoolBinary(m, BINOP(!=), Module::NE_MAGIC_NAME));
+  registerRule("int-constant-gt", intToBoolBinary(m, BINOP(>), Module::GT_MAGIC_NAME));
+  registerRule("int-constant-ge", intToBoolBinary(m, BINOP(>=), Module::LT_MAGIC_NAME));
+  registerRule("int-constant-lt", intToBoolBinary(m, BINOP(<), Module::LT_MAGIC_NAME));
+  registerRule("int-constant-le", intToBoolBinary(m, BINOP(<=), Module::LT_MAGIC_NAME));
+
+  // unary, single constant, int->int
+  registerRule("int-constant-pos", intToIntUnary(m, UNOP(+), Module::POS_MAGIC_NAME));
+  registerRule("int-constant-neg", intToIntUnary(m, UNOP(-), Module::POS_MAGIC_NAME));
+  registerRule("int-constant-inv",
+               intToIntUnary(m, UNOP(~), Module::INVERT_MAGIC_NAME));
 }
 
 } // namespace folding
 } // namespace transform
 } // namespace ir
 } // namespace seq
+
+#undef BINOP
+#undef UNOP
--- compiler/sir/transform/folding/rule.h
+++ compiler/sir/transform/folding/rule.h
@@ -22,14 +22,15 @@ public:
   virtual Value *apply(CallInstr *v) = 0;
 };
 
-/// Commutative, binary rule that matches a single constant value.
-template <typename T> class SingleConstantCommutativeRule : public FoldingRule {
+/// Commutative, binary rule that requires a single constant.
+template <typename ConstantType>
+class SingleConstantCommutativeRule : public FoldingRule {
 public:
   using Calculator = std::function<Value *(Value *)>;
 
 private:
   /// the value being matched against
-  T val;
+  ConstantType val;
   /// the type being matched
   types::Type *type;
   /// the magic method name
@@ -46,11 +47,12 @@ public:
   /// @param magic the magic name
   /// @param i 0 for left, 1 for right, -1 if commutative
   /// @param type the matched type
-  SingleConstantCommutativeRule(T val, T newVal, std::string magic, int i,
-                                types::Type *type)
+  SingleConstantCommutativeRule(ConstantType val, ConstantType newVal,
+                                std::string magic, int i, types::Type *type)
       : val(val), type(type), magic(std::move(magic)), i(i) {
     calc = [=](Value *v) -> Value * {
-      return v->getModule()->Nr<TemplatedConst<T>>(val, type);
+      return v->getModule()->N<TemplatedConst<ConstantType>>(v->getSrcInfo(), val,
+                                                             type);
     };
   }
   /// Constructs a commutative rule.
@@ -60,8 +62,8 @@ public:
   /// @param calc the calculator
   /// @param i 0 for left, 1 for right, -1 if commutative
   /// @param type the matched type
-  SingleConstantCommutativeRule(T val, std::string magic, Calculator calc, int i,
-                                types::Type *type)
+  SingleConstantCommutativeRule(ConstantType val, Calculator calc, std::string magic,
+                                int i, types::Type *type)
       : val(val), type(type), magic(std::move(magic)), calc(std::move(calc)), i(i) {}
 
   virtual ~SingleConstantCommutativeRule() noexcept = default;
@@ -84,8 +86,8 @@ public:
         right->getType()->getName() != type->getName())
       return nullptr;
 
-    auto *leftConst = cast<TemplatedConst<T>>(left);
-    auto *rightConst = cast<TemplatedConst<T>>(right);
+    auto *leftConst = cast<TemplatedConst<ConstantType>>(left);
+    auto *rightConst = cast<TemplatedConst<ConstantType>>(right);
 
     if ((i == -1 || i == 0) && leftConst && leftConst->getVal() == val)
       return calc(right);
@@ -93,6 +95,127 @@ public:
       return calc(left);
 
     return nullptr;
+  }
+};
+
+/// Binary rule that requires two constants.
+template <typename ConstantType, typename Func>
+class DoubleConstantBinaryRule : public FoldingRule {
+private:
+  /// the calculator
+  Func f;
+  /// the input type
+  types::Type *inputType;
+  /// the output type
+  types::Type *resultType;
+  /// the magic method name
+  std::string magic;
+
+public:
+  /// Constructs a binary rule.
+  /// @param f the calculator
+  /// @param magic the magic method name
+  /// @param inputType the input type
+  /// @param resultType the output type
+  DoubleConstantBinaryRule(Func f, std::string magic, types::Type *inputType,
+                           types::Type *resultType)
+      : f(std::move(f)), inputType(inputType), resultType(resultType),
+        magic(std::move(magic)) {}
+
+  virtual ~DoubleConstantBinaryRule() noexcept = default;
+
+  Value *apply(CallInstr *v) override {
+    auto *fn = util::getFunc(v->getCallee());
+    if (!fn)
+      return nullptr;
+
+    if (fn->getUnmangledName() != magic)
+      return nullptr;
+
+    if (std::distance(v->begin(), v->end()) != 2)
+      return nullptr;
+
+    auto *left = v->front();
+    auto *right = v->back();
+
+    if (left->getType()->getName() != inputType->getName() ||
+        right->getType()->getName() != inputType->getName())
+      return nullptr;
+
+    auto *leftConst = cast<TemplatedConst<ConstantType>>(left);
+    auto *rightConst = cast<TemplatedConst<ConstantType>>(right);
+
+    if (leftConst && rightConst)
+      return toValue(v, f(leftConst->getVal(), rightConst->getVal()));
+
+    return nullptr;
+  }
+
+private:
+  Value *toValue(Value *, Value *v) { return v; }
+
+  template <typename NewType> Value *toValue(Value *og, NewType v) {
+    return og->getModule()->template N<TemplatedConst<NewType>>(og->getSrcInfo(), v,
+                                                                resultType);
+  }
+};
+
+/// Unary rule that requires one constant.
+template <typename ConstantType, typename Func>
+class SingleConstantUnaryRule : public FoldingRule {
+private:
+  /// the calculator
+  Func f;
+  /// the input type
+  types::Type *inputType;
+  /// the output type
+  types::Type *resultType;
+  /// the magic method name
+  std::string magic;
+
+public:
+  /// Constructs a unary rule.
+  /// @param f the calculator
+  /// @param magic the magic method name
+  /// @param inputType the input type
+  /// @param resultType the output type
+  SingleConstantUnaryRule(Func f, std::string magic, types::Type *inputType,
+                          types::Type *resultType)
+      : f(std::move(f)), inputType(inputType), resultType(resultType),
+        magic(std::move(magic)) {}
+
+  virtual ~SingleConstantUnaryRule() noexcept = default;
+
+  Value *apply(CallInstr *v) override {
+    auto *fn = util::getFunc(v->getCallee());
+    if (!fn)
+      return nullptr;
+
+    if (fn->getUnmangledName() != magic)
+      return nullptr;
+
+    if (std::distance(v->begin(), v->end()) != 1)
+      return nullptr;
+
+    auto *arg = v->front();
+
+    if (arg->getType()->getName() != inputType->getName())
+      return nullptr;
+
+    auto *argConst = cast<TemplatedConst<ConstantType>>(arg);
+
+    if (argConst)
+      return toValue(v, f(argConst->getVal()));
+
+    return nullptr;
+  }
+
+private:
+  Value *toValue(Value *, Value *v) { return v; }
+
+  template <typename NewType> Value *toValue(Value *og, NewType v) {
+    return og->getModule()->template N<TemplatedConst<NewType>>(og->getSrcInfo(), v,
+                                                                resultType);
   }
 };
 
