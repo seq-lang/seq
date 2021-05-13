2d5a9565ce1dd15cc31cef6bfa0936108b54a072
Gabriel L Ramirez <gabe.luis.ramirez@gmail.com>
2021-05-13 15:51:39 -0600

    agressive

--- compiler/sir/util/inlining.cpp
+++ compiler/sir/util/inlining.cpp
@@ -13,20 +13,49 @@ class ReturnReplacer : public util::Operator {
 private:
   Value *implicitLoop;
   Var *var;
+  bool agressive;
   util::CloneVisitor &cv;
 
 public:
-  ReturnReplacer(Value *implicitLoop, Var *var, util::CloneVisitor &cv)
-      : implicitLoop(implicitLoop), var(var), cv(cv) {}
+  bool ok = true;
+
+  ReturnReplacer(Value *implicitLoop, Var *var, bool agressive, util::CloneVisitor &cv)
+      : implicitLoop(implicitLoop), var(var), agressive(agressive), cv(cv) {}
 
   void handle(ReturnInstr *v) {
+    if (!verifyReturn(v)) {
+      ok = false;
+      return;
+    }
+
     auto *M = v->getModule();
     auto *rep = M->N<SeriesFlow>(v);
     if (var) {
       rep->push_back(M->N<AssignInstr>(v, var, cv.clone(v->getValue())));
     }
-    rep->push_back(M->N<BreakInstr>(v, implicitLoop));
+    if (agressive)
+      rep->push_back(M->N<BreakInstr>(v, implicitLoop));
+
     v->replaceAll(rep);
+  }
+
+private:
+  bool verifyReturn(ReturnInstr *v) {
+    if (agressive)
+      return true;
+
+    auto it = parent_begin();
+    if (it == parent_end())
+      return false;
+
+    SeriesFlow *prev = nullptr;
+    while (it != parent_end()) {
+      auto *cur = cast<SeriesFlow>(*it++);
+      if (!cur || (prev && prev->back()->getId() != cur->getId()))
+        return false;
+      prev = cur;
+    }
+    return prev->back()->getId() == v->getId();
   }
 };
 
@@ -36,7 +65,8 @@ namespace seq {
 namespace ir {
 namespace util {
 
-InlineResult inlineFunction(Func *func, std::vector<Value *> args, seq::SrcInfo info) {
+InlineResult inlineFunction(Func *func, std::vector<Value *> args, bool agressive,
+                            seq::SrcInfo info) {
   auto *bodied = cast<BodiedFunc>(func);
   auto *fType = cast<types::FuncType>(bodied->getType());
   if (!bodied || args.size() != std::distance(bodied->arg_begin(), bodied->arg_end()))
@@ -62,21 +92,31 @@ InlineResult inlineFunction(Func *func, std::vector<Value *> args, seq::SrcInfo 
   }
 
   auto *clonedBody = cv.clone(bodied->getBody());
-  auto *implicitLoop = M->N<WhileFlow>(info, M->getBool(true), clonedBody);
-  cast<SeriesFlow>(clonedBody)->push_back(M->N<BreakInstr>(info, implicitLoop));
-  ReturnReplacer rr(implicitLoop, retVal, cv);
+
+  WhileFlow *implicit = nullptr;
+  if (agressive) {
+    implicit = M->N<WhileFlow>(info, M->getBool(true), clonedBody);
+    if (!retVal)
+      cast<SeriesFlow>(clonedBody)->push_back(M->N<BreakInstr>(info, implicit));
+  }
+
+  ReturnReplacer rr(implicit, retVal, agressive, cv);
   rr.process(clonedBody);
 
-  Value *v = implicitLoop;
+  if (!rr.ok)
+    return {false, nullptr, {}};
+
+  Value *v = implicit ? implicit : clonedBody;
   if (retVal) {
-    v = M->N<FlowInstr>(info, implicitLoop, M->N<VarValue>(info, retVal));
+    v = M->N<FlowInstr>(info, cast<Flow>(v), M->N<VarValue>(info, retVal));
   }
   return {true, v, std::move(newVars)};
 }
 
-InlineResult inlineCall(CallInstr *v) {
+InlineResult inlineCall(CallInstr *v, bool agressive) {
   return inlineFunction(util::getFunc(v->getCallee()),
-                        std::vector<Value *>(v->begin(), v->end()), v->getSrcInfo());
+                        std::vector<Value *>(v->begin(), v->end()), agressive,
+                        v->getSrcInfo());
 }
 
 } // namespace util
--- compiler/sir/util/inlining.h
+++ compiler/sir/util/inlining.h
@@ -20,13 +20,16 @@ struct InlineResult {
 /// @param func the function
 /// @param args the arguments
 /// @param callInfo the call information
+/// @param aggressive true if should inline complex functions
 /// @return the inlined result, nullptr if unsuccessful
 InlineResult inlineFunction(Func *func, std::vector<Value *> args,
-                            seq::SrcInfo callInfo = {});
+                            bool aggressive = false, seq::SrcInfo callInfo = {});
 
 /// Inline the given call.
+/// @param v the instruction
+/// @param aggressive true if should inline complex functions
 /// @return the inlined result, nullptr if unsuccessful
-InlineResult inlineCall(CallInstr *v);
+InlineResult inlineCall(CallInstr *v, bool aggressive = false);
 
 } // namespace util
 } // namespace ir
