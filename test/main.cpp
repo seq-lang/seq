bb8c99e59f83adb93b4006ae4a9472d07a374eaa
A. R. Shajii <ars@ars.me>
2021-06-06 23:27:47 -0400

    Add operator module

--- test/main.cpp
+++ test/main.cpp
@@ -377,6 +377,7 @@ INSTANTIATE_TEST_SUITE_P(
         "stdlib/statistics_test.seq",
         "stdlib/sort_test.seq",
         "stdlib/heapq_test.seq",
+        "stdlib/operator_test.seq",
         "python/pybridge.seq"
       ),
       testing::Values(true, false),
