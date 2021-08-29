15043f503288711cce8d980934a4550563ab3e61
A. R. Shajii <ars@ars.me>
2021-08-29 01:23:54 -0400

    Fix CFG

--- compiler/sir/analyze/dataflow/cfg.cpp
+++ compiler/sir/analyze/dataflow/cfg.cpp
@@ -140,6 +140,15 @@ std::unique_ptr<Result> CFAnalysis::run(const Module *m) {
   return res;
 }
 
+void CFVisitor::visit(const BodiedFunc *f) {
+  auto *blk = graph->getCurrentBlock();
+  for (auto it = f->arg_begin(); it != f->arg_end(); it++) {
+    blk->push_back(graph->N<analyze::dataflow::SyntheticAssignInstr>(
+        const_cast<Var *>(*it), const_cast<VarValue *>(graph->N<VarValue>(*it))));
+  }
+  process(f->getBody());
+}
+
 void CFVisitor::visit(const SeriesFlow *v) {
   for (auto *c : *v) {
     process(c);
@@ -224,10 +233,11 @@ void CFVisitor::visit(const ImperativeForFlow *v) {
 
   auto *loopBegin = graph->newBlock("forBegin", true);
   original->successors_insert(loopBegin);
-  process(v->getStart());
   loopBegin->push_back(graph->N<analyze::dataflow::SyntheticAssignInstr>(
       const_cast<Var *>(v->getVar()), const_cast<Value *>(v->getStart()),
       analyze::dataflow::SyntheticAssignInstr::KNOWN));
+  process(v->getStart());
+  process(v->getEnd());
 
   auto *loopCheck = graph->newBlock("forCheck");
   graph->getCurrentBlock()->successors_insert(loopCheck);
@@ -384,7 +394,9 @@ void CFVisitor::visit(const ThrowInstr *v) {
 
 void CFVisitor::visit(const FlowInstr *v) {
   process(v->getFlow());
-  process(v);
+  if (v->getValue())
+    process(v->getValue());
+  defaultInsert(v);
 }
 
 void CFVisitor::visit(const dsl::CustomInstr *v) {
