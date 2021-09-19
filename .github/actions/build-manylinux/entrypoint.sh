9c639c944ff510ab0c5d3fe2aadddd34ad252ebb
A. R. Shajii <ars@ars.me>
2021-09-18 22:37:05 -0400

    Fix CI

--- .github/actions/build-manylinux/entrypoint.sh
+++ .github/actions/build-manylinux/entrypoint.sh
@@ -1,28 +1,34 @@
 #!/bin/sh -l
-
 set -e
 
 # setup
 cd /github/workspace
-apt-get update
-apt-get -y --force-yes install build-essential sudo curl wget git zlib1g-dev libbz2-dev liblzma-dev python-software-properties apt-transport-https ca-certificates python3.6
+yum -y update
+yum -y install wget python3
 
-# llvm
-wget https://apt.llvm.org/llvm.sh
-chmod +x llvm.sh
-sudo ./llvm.sh 12
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
+bash miniconda.sh -b -p miniconda
+eval "$(miniconda/bin/conda shell.bash hook)"
+conda init
+conda install -y -c conda-forge clang-12
 
 # env
-export CC=clang-12
-export CXX=clang++-12
+cp miniconda/bin/clang-cpp miniconda/bin/clang++
+export CC=clang
+export CXX=clang++
 export PYTHONPATH=$(pwd)/test/python
-export SEQ_PYTHON=$(python3.6 test/python/find-python-library.py)
-python3.6 -m pip install numpy
+export SEQ_PYTHON=$(python3 test/python/find-python-library.py)
+python3 -m pip install numpy
+
+# deps
+if [ ! -d ./llvm ]; then
+  /bin/bash scripts/deps.sh 2;
+fi
 
 # build
 mkdir build
 (cd build && cmake .. -DCMAKE_BUILD_TYPE=Release \
-                      -DLLVM_DIR=$(llvm-config-12 --cmakedir) \
+                      -DLLVM_DIR=$(llvm/bin/llvm-config --cmakedir) \
                       -DCMAKE_C_COMPILER=${CC} \
                       -DCMAKE_CXX_COMPILER=${CXX})
 cmake --build build --config Release -- VERBOSE=1
@@ -35,14 +41,10 @@ build/seqc run test/core/exit.seq || if [[ $? -ne 42 ]]; then false; fi
 
 # package
 export SEQ_BUILD_ARCHIVE=seq-$(uname -s | awk '{print tolower($0)}')-$(uname -m).tar.gz
-export SEQ_DEP_ARCHIVE=seq-deps-linux.tar.bz2
 mkdir -p seq-deploy/bin seq-deploy/lib/seq
 cp build/seqc seq-deploy/bin/
 cp build/libseq*.so seq-deploy/lib/seq/
-cp deps/lib/libomp.so seq-deploy/lib/seq/
+cp build/libomp.so seq-deploy/lib/seq/
 cp -r stdlib seq-deploy/lib/seq/
 tar -czf ${SEQ_BUILD_ARCHIVE} seq-deploy
-tar -cjf ${SEQ_DEP_ARCHIVE} deps
 du -sh seq-deploy
-du -sh deps
-ls -lah ${SEQ_DEP_ARCHIVE}
--- scripts/deps.sh
+++ scripts/deps.sh
@@ -1,46 +1,35 @@
 #!/usr/bin/env bash
-
 set -e
 
 export CC="${CC:-clang}"
 export CXX="${CXX:-clang++}"
-export USE_ZLIBNG="${USE_ZLIBNG:-1}"
 
-export INSTALLDIR="${PWD}/deps"
-export SRCDIR="${PWD}/deps_src"
+export INSTALLDIR="llvm"
+export SRCDIR="llvm-project"
 mkdir -p "${INSTALLDIR}" "${SRCDIR}"
-
-export LD_LIBRARY_PATH="${INSTALLDIR}/lib:${LD_LIBRARY_PATH}"
-export LIBRARY_PATH="${INSTALLDIR}/lib:${LIBRARY_PATH}"
-export CPATH="${INSTALLDIR}/include:${CPATH}"
-
-die() { echo "$*" 1>&2 ; exit 1; }
 
 export JOBS=1
 if [ -n "${1}" ]; then export JOBS="${1}"; fi
 echo "Using ${JOBS} cores..."
 
-# Tapir
-TAPIR_LLVM_BRANCH='release_60-release'
-if [ ! -d "${SRCDIR}/Tapir-LLVM" ]; then
-  git clone --depth 1 -b "${TAPIR_LLVM_BRANCH}" https://github.com/seq-lang/Tapir-LLVM "${SRCDIR}/Tapir-LLVM"
+LLVM_BRANCH="release/12.x"
+if [ ! -d "${SRCDIR}" ]; then
+  git clone --depth 1 -b release/12.x https://github.com/llvm/llvm-project "${SRCDIR}"
 fi
-if [ ! -f "${INSTALLDIR}/bin/llc" ]; then
-  mkdir -p "${SRCDIR}/Tapir-LLVM/build"
-  cd "${SRCDIR}/Tapir-LLVM/build"
+if [ ! -f "${INSTALLDIR}/bin/llvm-config" ]; then
+  mkdir -p "${SRCDIR}/llvm/build"
+  cd "${SRCDIR}/llvm/build"
   cmake .. \
-     -DLLVM_INCLUDE_TESTS=OFF \
-     -DLLVM_ENABLE_RTTI=ON \
-     -DCMAKE_BUILD_TYPE=Release \
-     -DLLVM_TARGETS_TO_BUILD=host \
-     -DLLVM_ENABLE_ZLIB=OFF \
-     -DLLVM_ENABLE_TERMINFO=OFF \
-     -DCMAKE_C_COMPILER="${CC}" \
-     -DCMAKE_CXX_COMPILER="${CXX}" \
-     -DCMAKE_INSTALL_PREFIX="${INSTALLDIR}"
+      -DLLVM_INCLUDE_TESTS=OFF \
+      -DLLVM_ENABLE_RTTI=ON \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DLLVM_TARGETS_TO_BUILD=host \
+      -DLLVM_ENABLE_ZLIB=OFF \
+      -DLLVM_ENABLE_TERMINFO=OFF \
+      -DCMAKE_C_COMPILER="${CC}" \
+      -DCMAKE_CXX_COMPILER="${CXX}" \
+      -DCMAKE_INSTALL_PREFIX="${INSTALLDIR}"
   make -j "${JOBS}"
   make install
   "${INSTALLDIR}/bin/llvm-config" --cmakedir
 fi
-
-echo "Dependency generation done: ${INSTALLDIR}"
