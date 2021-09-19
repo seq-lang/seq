9b5536d139585f63d2ad306a4189f0b42a488992
A. R. Shajii <ars@ars.me>
2021-09-19 11:55:31 -0400

    Remove unused package

--- .github/actions/build-manylinux/entrypoint.sh
+++ .github/actions/build-manylinux/entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 # setup
 cd /github/workspace
 yum -y update
-yum -y install centos-release-scl wget python3 llvm-toolset-7
+yum -y install wget python3 llvm-toolset-7
 
 # env
 export CC=/opt/rh/llvm-toolset-7/root/usr/bin/clang
